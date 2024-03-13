43d0d9e27ba0f2de197590e272a520f995cfa24d
Ken <nontsuke.ken@gmail.com>
2024-03-13 13:31:07 -0400

    test of concept done

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//Inspired by https://editor.p5js.org/manno/sketches/aqwj6Y9qW VU meter 
+
 // size of the rect representing volume
 let meterSize = 0;
 // the max level registered
@@ -5,6 +7,7 @@ let maxVol = 0;
 let mic;
 let VU;
 let meterColor;
+let txtAlert;
 function setup() {
     let cnv = createCanvas(windowWidth, windowHeight);
     mic = new p5.AudioIn();
@@ -16,9 +19,9 @@ function setup() {
 function draw() {
     background(220);
     
-    // contrary to online documentation, this returns values way above 1.0 in Safari
-    // (with or without the smoothing parameter)
-    let volume = mic.getLevel();
+    frameRate(120)
+
+    let volume = mic.getLevel(0);
     
     // set width of rect representing volume equal to: 
     // the current volume * width OR previous width -1 (whichever is larger)
@@ -26,12 +29,15 @@ function draw() {
     VU = rect(0, height, windowWidth, -meterSize * 3);
     if(volume > 0.220){
         meterColor = "red"
+        txtAlert = "Too Loud"
     }
     else if(volume <= 0.220 && volume >= 0.150){
         meterColor = "yellow"
+        txtAlert = "Loud"
     }
     else{
         meterColor = 'green'
+        txtAlert = "OK"
     }
     fill(meterColor)
     noStroke()
@@ -39,6 +45,7 @@ function draw() {
     // output numerical value of highest reading
     maxVol = max(maxVol, volume);
     text(volume, 10, 10);
+    text(txtAlert, 20,20);
 
 
   //resize Canvas when window change
